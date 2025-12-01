57f26051801dd4730d82b77bdc182f38beab1c7a
domas85 <domasp2002@gmail.com>
2025-12-01 09:13:28 +0100

    first init

---
+++ AdventOfCode2025/Day1/Day1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AOC
+{
+    public class Day1
+    {
+        public static string line = "";
+
+        public static void Run()
+        {
+            Console.Write("Starting...");
+            ReadInput();
+            SolvePart1();
+            SolvePart2();
+        }
+
+        public static void SolvePart2()
+        {
+        
+        }
+
+        public static void SolvePart1()
+        {
+   
+        }
+
+        public static void ReadInput()
+        {
+            StreamReader sr = new StreamReader("C:\\files\\projects\\Advent-Of-Code-2025\\AdventOfCode2025\\Day1\\Input.txt");
+            while (line != null)
+            {
+                line = sr.ReadLine();
+
+                Console.WriteLine("\n " + line);
+            }
+        }
+    }
+}
---
+++ AdventOfCode2025/Program.cs
@@ -0,0 +1,21 @@
+﻿using AOC;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Day1.Run();
+        //Day2.Run();
+        //Day3.Run();
+        //Day4.Run();
+        //Day5.Run();
+        //Day6.Run();
+        //Day7.Run();
+        //Day8.Run();
+        //Day9.Run();
+        //Day10.Run();
+        //Day11.Run();
+        //Day12.Run();
+    }
+}
